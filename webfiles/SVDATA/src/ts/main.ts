fc81c5b1a8e1afadecaa2d5b637d04e5db4c2526
Tim Goll <tim@timgoll.de>
2019-07-13 01:57:25 +0200

    added some initial tests

--- webfiles/SVDATA/src/ts/main.ts
+++ webfiles/SVDATA/src/ts/main.ts
@@ -1,10 +1,24 @@
-class Main {
-    constructor() {
+import { ContentHandler } from "./content_handler";
 
+class Main {
+    content_handler : ContentHandler;
+
+    constructor() {
+        this.content_handler = new ContentHandler();
     };
 
     init() : void {
-        console.log("Hello World");
+        // add templates
+        this.content_handler.registerTemplate('home', {
+            template_url: 'SVDATA/data/templates/home.html',
+            elelement_id: 'content'
+        });
+
+        // init the content manager 
+        this.content_handler.init({
+            homedir : 'home',
+            error_path: 'SVDATA/data/templates/error404.html'
+        });
     };
 }
 
