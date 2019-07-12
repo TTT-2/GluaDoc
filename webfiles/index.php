d7ac5beb85c76f36fe9267e0eccc18bc364b767b
Tim Goll <tim@timgoll.de>
2019-07-12 20:21:49 +0200

    further styling improvements

--- webfiles/index.php
+++ webfiles/index.php
@@ -1,83 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-		$f = file_get_contents("documentation.json");
-		$funcs = json_decode($f, true);
+	$f = file_get_contents("documentation.json");
+	$funcs = json_decode($f, true);
 
-		//strip undocumented
-		foreach($funcs as $n => $data)
+	//strip undocumented
+	foreach($funcs as $n => $data)
+	{
+		$funcs[$n] = count($data["param"]) > 0 ? $funcs[$n] : false;
+
+		if(!$funcs[$n] || isset($funcs[$n]["param"]["local"])) // exclude unset functions or @local functions
 		{
-				$funcs[$n] = count($data["param"]) > 0 ? $funcs[$n] : false;
-
-				if(!$funcs[$n] || isset($funcs[$n]["param"]["local"])) // exclude unset functions or @local functions
-				{
-						unset($funcs[$n]);
-				}
-				else
-				{
-						if (!isset($data["param"]["realm"]))
-						{
-								$funcs[$n]["param"]["realm"] = "shared";
-						}
-				}
+				unset($funcs[$n]);
 		}
+		else
+		{
+			if (!isset($data["param"]["realm"]))
+			{
+					$funcs[$n]["param"]["realm"] = "shared";
+			}
+		}
+	}
 ?>
 <head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" type="text/css" href="stylish.css">
+	<link rel="stylesheet" type="text/css" href="stylish.css">
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="script.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="script.js"></script>
 
-		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
 </head>
 <body>
-		<div id="navlist">
+	<div id="navlist">
+		<?php
+			foreach($funcs as $n => $data)
+			{
+				//preprocess name
+				$name_parts = explode('.', $data["name"]);
+				$name_print = '<a href=?func=' . $data["name"] . '><span class="navlist-element wrapper">';
+
+				for ($i = 0; $i < count($name_parts) - 1; $i++) {
+					$name_print .= '<span class="navlist-element prefix">' . $name_parts[$i];
+					if ($i < count($name_parts) -1) {
+						$name_print .= '.';
+					}
+					$name_print .= '</span>';
+				}
+				$name_print .= '<span class="navlist-element ' . strtolower($data["param"]["realm"]) . '">' . end($name_parts) . '</span></span>';
+				$name_print .= '</a>';
+
+				echo $name_print;
+			}
+		?>
+	</div>
+	<div id="code-container">
+		<div id="code">
 				<?php
+					if(isset($_GET) && isset($_GET["func"]))
+					{
 						foreach($funcs as $n => $data)
 						{
-								echo
-								'
-										<a href="?func=' . $data["name"] . '">
-												<span class="navlist-element ' . strtolower($data["param"]["realm"]) . '">
-														' . $data["name"] . '
-												</span>
-										</a>
-								';
+							if($data["name"] == $_GET["func"])
+							{
+								$i = $n;
+								break;
+							}
 						}
+					}
+
+					if(isset($i))
+					{
+						echo '<span class="code-funcname ' . strtolower($funcs[$i]["param"]["realm"]) . '">' . $funcs[$i]["name"] . '</span><span class="code-funcargs">( ' . (isset($funcs[$i]["param"]["args"]) ? $funcs[$i]["param"]["args"] : "" ) . ' )</span><br>';
+
+						if(isset($funcs[$i]["param"]["desc"]))
+						{
+							echo '<span class="code-desc">DESC: ' . $funcs[$i]["param"]["desc"] . '</span><br>';
+						}
+
+						echo '<span class="code-note">NOTE: ' . (isset($funcs[$i]["param"]["note"]) ? $funcs[$i]["param"]["note"] : "None" ) . '</span>';
+					}
 				?>
 		</div>
-		<div id="code-container">
-				<div id="code">
-						<?php
-								if(isset($_GET) && isset($_GET["func"]))
-								{
-										foreach($funcs as $n => $data)
-										{
-												if($data["name"] == $_GET["func"])
-												{
-														$i = $n;
-
-														break;
-												}
-										}
-								}
-
-								if(isset($i))
-								{
-										echo '<span class="code-funcname ' . strtolower($funcs[$i]["param"]["realm"]) . '">' . $funcs[$i]["name"] . '</span><span class="code-funcargs">( ' . (isset($funcs[$i]["param"]["args"]) ? $funcs[$i]["param"]["args"] : "" ) . ' )</span><br>';
-
-										if(isset($funcs[$i]["param"]["desc"]))
-										{
-												echo '<span class="code-desc">DESC: ' . $funcs[$i]["param"]["desc"] . '</span><br>';
-										}
-
-										echo '<span class="code-note">NOTE: ' . (isset($funcs[$i]["param"]["note"]) ? $funcs[$i]["param"]["note"] : "None" ) . '</span>';
-								}
-						?>
-				</div>
-		</div>
+	</div>
 </body>
 </html>
